1cbbec3659205989dac2b2fcfe154d0cb14b7684
nenadpejic <npejic89@gmail.com>
2020-08-26 10:23:28 +0200

    Moved difficultyCards arr to separate js file

---
+++ difficulty.js
@@ -0,0 +1,19 @@
+// Difficulty
+let difficultyCards = [
+  {
+    difficulty: "easy",
+    cards: 16
+  },
+  {
+    difficulty: "medium",
+    cards: 36
+  },
+  {
+    difficulty: "hard",
+    cards: 64
+  },
+  {
+    difficulty: "expert",
+    cards: 100
+  },
+];
--- script.js
+++ script.js
@@ -19,26 +19,6 @@ inputUsername.addEventListener("keydown", function () {
   }
 });
 
-// Difficulty
-let difficultyCards = [
-  {
-    difficulty: "easy",
-    cards: 16
-  },
-  {
-    difficulty: "medium",
-    cards: 36
-  },
-  {
-    difficulty: "hard",
-    cards: 64
-  },
-  {
-    difficulty: "expert",
-    cards: 100
-  },
-];
-
 // Table
 let createTable = () => {
   // Get difficulty
